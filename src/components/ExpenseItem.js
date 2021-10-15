84bcec9ddc81ef651816afec143722b5b0893e99
Your Name <you@example.com>
2021-10-15 12:29:07 +0530

    added some components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,18 @@
-function ExpenseItem(){
-    return(
-        <div>
-            <h2>This is a sample heading!</h2>
-        </div>
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+function ExpenseItem(props) {
+    // const expenseDate = new Date(2021,2,28);
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67;
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date}/>
+            <div className='expense-item__description'>
+                <h2>{props.title} </h2>
+                <div className='expense-item__price'>${props.amount} </div>
+            </div>
+        </Card>
     );
 }
 
